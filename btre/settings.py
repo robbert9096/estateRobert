dce534493eda2cd202a926fc5c26f4a03584350a
Robert Costache <robertcostache62@gmail.com>
2022-09-11 20:33:31 +0100

    without static

--- btre/settings.py
+++ btre/settings.py
@@ -120,12 +120,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIR = BASE_DIR / 'static'
-
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.CachedStaticFilesStorage'
+
+
+
+STATIC_URL = '/static/'
+STATICFILES_DIR = [
+    BASE_DIR / 'static'
+]
 #Messages
 from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
